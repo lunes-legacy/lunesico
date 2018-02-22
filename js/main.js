a41c6a0f016a104988da93e4f84ab2c503d45e58
davidalves1 <alves.david@outlook.com>
2018-02-22 19:17:09 -0300

    Corrigir dados do roadmap e corrigir textos do site em inglês

--- js/main.js
+++ js/main.js
@@ -1,80 +1,76 @@
-
 //AOS.init();
 
-var toggleMenu = function(){
-    $("#navbar").toggle('fast');
-}
+var toggleMenu = function() {
+  $('#navbar').toggle('fast');
+};
 
-$("#navbar").click(function(){
-    toggleMenu();
+$('#navbar').click(function() {
+  toggleMenu();
 });
 
-function showTab(item){
-    if(item=="qte"){
-        $("#qte").show();
-        $("#per").hide();
+function showTab(item) {
+  if (item == 'qte') {
+    $('#qte').show();
+    $('#per').hide();
 
-        $("#tab_qte").addClass("ativo");
-        $("#tab_per").removeClass("ativo");
-    }else{
-        $("#qte").hide();
-        $("#per").show();
+    $('#tab_qte').addClass('ativo');
+    $('#tab_per').removeClass('ativo');
+  } else {
+    $('#qte').hide();
+    $('#per').show();
 
-        $("#tab_qte").removeClass("ativo");
-        $("#tab_per").addClass("ativo");
-    }
+    $('#tab_qte').removeClass('ativo');
+    $('#tab_per').addClass('ativo');
+  }
 }
 
-$("#navbar #cont a").click(function(){
-    $("#navbar #cont").find(".ativo").removeClass("ativo");
-    $(this).addClass("ativo");
+$('#navbar #cont a').click(function() {
+  $('#navbar #cont')
+    .find('.ativo')
+    .removeClass('ativo');
+  $(this).addClass('ativo');
 });
 
-$("#navbar #cont a").on('click', function(event) {
-    // var target = this.hash;
-
-    // event.preventDefault();
-
-    // var navOffset = 130;
-    
-    // return $('html, body').animate({
-    //     scrollTop: $(this.hash).offset().top - navOffset
-    // }, 500, function() {
-    //     return window.history.pushState(null, null, target);
-    // });
+$('#navbar #cont a').on('click', function(event) {
+  // var target = this.hash;
+  // event.preventDefault();
+  // var navOffset = 130;
+  // return $('html, body').animate({
+  //     scrollTop: $(this.hash).offset().top - navOffset
+  // }, 500, function() {
+  //     return window.history.pushState(null, null, target);
+  // });
 });
 
 /* time counter */
-function getTime(){
-    var dataInicio = new Date();
-    var dataFim = new Date("02/25/2018");
-    var diffMilissegundos = dataFim - dataInicio;
+function getTime() {
+  var dataInicio = new Date();
+  var dataFim = new Date('02/25/2018');
+  var diffMilissegundos = dataFim - dataInicio;
 
-    var dif = new Date(diffMilissegundos);
+  var dif = new Date(diffMilissegundos);
 
-    $("#con_days").html(dif.getDate());
-    $("#con_hours").html(dif.getHours());
-    $("#con_min").html(dif.getMinutes());
-    $("#con_sec").html(dif.getSeconds());
-    
+  $('#con_days').html(dif.getDate());
+  $('#con_hours').html(dif.getHours());
+  $('#con_min').html(dif.getMinutes());
+  $('#con_sec').html(dif.getSeconds());
 }
 setInterval(getTime, 1000);
 
-
 /* slide time */
 $(document).ready(function() {
-    $("#slide_time").lightSlider({
-        gallery:true,
-        item:1,
-        loop:true,
-        thumbItem:9,
-        slideMargin:0,
-        enableDrag: true,
-        currentPagerPosition:'left',
-        onSliderLoad: function(el) {
-            el.lightGallery({
-                selector: '#slide_time .lslide'
-            });
-        }   
-    });
+  $('#slide_time').lightSlider({
+    gallery: true,
+    item: 1,
+    loop: true,
+    thumbItem: 9,
+    slideMargin: 0,
+    enableDrag: true,
+    currentPagerPosition: 'left',
+    onSliderLoad: function(el) {
+      el.lightGallery({
+        selector: '#slide_time .lslide'
+      });
+    }
+  });
 });
