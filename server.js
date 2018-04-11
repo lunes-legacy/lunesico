f019a556eced2456c9abaa0abe65d301b6e87bed
Hellen FS <hellen.ferreira@lunes.io>
2018-04-11 18:43:20 -0300

    remove express server

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(
-    express.static(__dirname) //where your static content is located in your filesystem
-);
-app.listen(3002, function () {
-  console.log('Express server listening on port 3002!');
-});
