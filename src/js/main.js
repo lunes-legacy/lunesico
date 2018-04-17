4665f58b8fc9dff0d60a270acf0f12b3bd8464d5
Hellen FS <hellen.ferreira@lunes.io>
2018-04-17 14:56:34 -0300

    Add product illustration animation / Add hindi language

--- src/js/main.js
+++ src/js/main.js
@@ -151,21 +151,25 @@ $("#put_s_day_en").html('Days');
 $("#put_s_day_pt").html('Dias');
 $("#put_s_day_fr").html('Jours');
 $("#put_s_day_ar").html('أيام'); 
+$("#put_s_day_hi").html('दिन'); 
 
 $("#put_s_hour_en").html('Hours');
 $("#put_s_hour_pt").html('Horas');
 $("#put_s_hour_fr").html('Heures');
 $("#put_s_hour_ar").html('ساعات');
+$("#put_s_hour_hi").html('घंटे');
 
 $("#put_s_min_en").html('Minutes');
 $("#put_s_min_pt").html('Minutos');
 $("#put_s_min_fr").html('Minutes');
 $("#put_s_min_ar").html('دقائق');
+$("#put_s_min_hi").html('मिनट');
 
 $("#put_s_sec_en").html('Seconds');
 $("#put_s_sec_pt").html('Segundos');
 $("#put_s_sec_fr").html('Seconds');
 $("#put_s_sec_ar").html('ثواني');
+$("#put_s_sec_hi").html('सेकंड');
 
 // Contador
 function getDateTime(endDate) {
